9d7b8773d928f337e7b98fd83edb15b8a5bb07f2
JAAV30 <Jose@iMac-de-Jose.local>
2016-04-30 02:14:54 -0600

    Opciones de Respuesta
    Cambio de color al selección de respuesta

--- www/app/controllers/PreguntaController.js
+++ www/app/controllers/PreguntaController.js
@@ -5,7 +5,34 @@ function AppPregunta($scope,$ionicPlatform) {
   $ionicPlatform.ready(function() {
 
       console.log("Ready AppPregunta");
-      $scope.dueno="LuisMi y yo trabajamos aqui, mensaje por medio de scope de angular";
+      //$scope.dueno="LuisMi y yo trabajamos aqui, mensaje por medio de scope de angular";
+      $scope.buttonColor="button button-block button-positive";
+      var respuestaCorrecta=1;
+      var analizado=false;
+     $scope.RespuestaA="RESPUESTA A";//se carga de la base de datos
+     $scope.RespuestaB="RESPUESTA B";
+     $scope.RespuestaC="RESPUESTA C";
+     $scope.RespuestaD="RESPUESTA D";
+
+	$scope.colorClass=function(value){
+  		if(analizado){
+  			if(value==respuestaCorrecta){
+  				return "button button-block button-balanced";
+  			}
+  			else {
+  				return "button button-block button-assertive";
+  			}
+  		}
+  		return "button button-block button-positive";
+  	}
+
+  	$scope.checkAnswer=function(value){
+		analizado=true;
+		if(respuestaCorrecta == value){
+			showAlert();
+		}
+	}
+
   });
 
 
