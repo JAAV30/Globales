adba5e601aed091c52985f37bef8c23766166e0f
JAAV30 <Jose@iMac-de-Jose.local>
2016-04-30 19:06:29 -0600

    Html enunciado
    primeras pruebas de agregar el enunciado desde el js

--- www/app/controllers/PreguntaController.js
+++ www/app/controllers/PreguntaController.js
@@ -7,13 +7,45 @@ function AppPregunta($scope,$ionicPlatform) {
       console.log("Ready AppPregunta");
       //$scope.dueno="LuisMi y yo trabajamos aqui, mensaje por medio de scope de angular";
       $scope.buttonColor="button button-block button-positive";
-      var respuestaCorrecta=1;
+      var respuestaCorrecta=3;
       var analizado=false;
-     $scope.RespuestaA="RESPUESTA A";//se carga de la base de datos
-     $scope.RespuestaB="RESPUESTA B";
-     $scope.RespuestaC="RESPUESTA C";
-     $scope.RespuestaD="RESPUESTA D";
+     
+     //se carga de la base de datos
+    $scope.encabezado="encabezado";
+    $scope.enunciado="enunciado";
+    $scope.imagen="img";
+    $scope.encabezadoProposiciones="Encabezado Proposiciones";
+    $scope.listaProposiciones=["uno","dos","tres"];
+    $scope.cuerpoPregunta="AQUI LA PREGUNTA";
+    $scope.RespuestaA="RESPUESTA A";
+    $scope.RespuestaB="RESPUESTA B";
+    $scope.RespuestaC="RESPUESTA C";
+    $scope.RespuestaD="RESPUESTA D";
+    $scope.enunciado=createHTML();;
 
+  function createHTML(){
+    
+    var texto="";
+      if($scope.encabezado != null){
+        texto=texto+"<p>"+$scope.encabezado+"</p><br>";  
+      }
+      if($scope.enunciado != null){
+        texto=texto+"<p>"+$scope.enunciado+"</p><br>";  
+      }
+      if($scope.encabezadoProposiciones != null){
+        texto=texto+"<p>"+$scope.encabezadoProposiciones+"</p><br>";  
+      }
+      if($scope.listaProposiciones.length > 0){
+        texto=texto+"<p><ol>";
+        for (var i = 0; i < $scope.listaProposiciones.length; i++) {
+          texto=texto+"<li>"+$scope.listaProposiciones[i]+"</li>";
+        };
+        texto=texto+"</ol></p>";
+      }
+      console.log(texto);
+      return texto;
+  }
+  
 	$scope.colorClass=function(value){
   		if(analizado){
   			if(value==respuestaCorrecta){
@@ -25,8 +57,8 @@ function AppPregunta($scope,$ionicPlatform) {
   		}
   		return "button button-block button-positive";
   	}
-
-  	$scope.checkAnswer=function(value){
+ 
+  $scope.checkAnswer=function(value){
 		analizado=true;
 		if(respuestaCorrecta == value){
 			showAlert();
