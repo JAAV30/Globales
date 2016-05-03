3c0227b4bf5f6d5e5c73eb9bcff60daf12aa4bb5
jodafm <josedani.04.24@gmail.com>
2016-05-03 00:45:35 -0600

    Correccion a anterior version...

--- www/app/controllers/EstadisticasController.js
+++ www/app/controllers/EstadisticasController.js
@@ -1,6 +1,6 @@
-angular.module('app').controller('EstadisticasController',['$scope','$ionicPlatform','QuestionsService',EstadisticasController]);
+angular.module('app').controller('EstadisticasController',['$scope','$ionicPlatform','$ionicModal','QuestionsService',EstadisticasController]);
 
-function EstadisticasController($scope,$ionicPlatform,service) {
+function EstadisticasController($scope,$ionicPlatform,$ionicModal,service) {
 
   $ionicPlatform.ready(function() {
 
@@ -11,6 +11,32 @@ function EstadisticasController($scope,$ionicPlatform,service) {
       $scope.$apply(function(){
         $scope.currentPlayer = service.getCurrentPlayer();
       });
+  });
+
+  $ionicModal.fromTemplateUrl('edit-player.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.showEditPlayerModal = function() {
+    $scope.action = 'Edit';
+    $scope.isAdd = false;
+    $scope.modal.show();
+  };
+
+  $scope.savePlayer = function() {
+
+    $scope.modal.hide();
+  };
+
+  $scope.deletePlayer = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
   });
 
 
