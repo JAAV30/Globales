f0d438c97123aebef26c56554dd5a1c513e0d189
josuech28 <josuech28@gmail.com>
2016-05-02 22:50:39 -0600

    Josue Estadisticas
    Intento de grafcas

--- www/app/controllers/EstadisticasController.js
+++ www/app/controllers/EstadisticasController.js
@@ -1,11 +1,18 @@
-angular.module('app').controller('EstadisticasController',['$scope','$ionicPlatform',EstadisticasController]);
+angular.module('app').controller('EstadisticasController',['$scope','$ionicPlatform','ionic', 'chart.js',EstadisticasController]);
 
-function EstadisticasController($scope,$ionicPlatform) {
+function EstadisticasController($scope,$ionicPlatform,$ionic,$chart.js) {
 
   $ionicPlatform.ready(function() {
 
       console.log("Ready EstadisticasController");
       $scope.dueno="Josue trabaje aqui, mensaje por medio de scope de angular";
+
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Series A', 'Series B'];
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+        ];
   });
 
 
